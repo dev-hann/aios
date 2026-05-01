77d15d12e2a75deb1c636238f04f1fea505f9606
hann (한) <developer.hann@gmail.com>
2026-05-01 13:03:37 +0000

    chore: bump to v1.4.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.agent.aios"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10400
-        versionName = "1.4.0"
+        versionCode = 10401
+        versionName = "1.4.1"
 
         buildConfigField("String", "GITHUB_REPO", "\"dev-hann/aios\"")
 
