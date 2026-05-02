c2529a14473843c641f71cd1d02fed8bc37abcb8
hann (한) <developer.hann@gmail.com>
2026-05-02 22:05:32 +0000

    release: v1.8.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.agent.aios"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10801
-        versionName = "1.8.1"
+        versionCode = 10802
+        versionName = "1.8.2"
 
         buildConfigField("String", "GITHUB_REPO", "\"dev-hann/aios\"")
 
